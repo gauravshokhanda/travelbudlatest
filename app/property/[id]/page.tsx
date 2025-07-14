849080ec7d8f12d7ba3056518e471ae697d24c0d
Gaurav Shokhanda <gauravshokhanda2@gmail.com>
2025-07-14 11:47:40 +0530

    new

--- app/property/[id]/page.tsx
+++ app/property/[id]/page.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
-
-export const page = () => {
+export default function PropertyPage({ params }: { params: { id: string } }) {
   return (
-    <div>page</div>
-  )
+    <div>
+      Property ID: {params.id}
+    </div>
+  );
 }
--- app/stays/[city]/page.tsx
+++ app/stays/[city]/page.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
-
-export const page = () => {
+export default function PropertyPage({ params }: { params: { id: string } }) {
   return (
-    <div>page</div>
-  )
+    <div>
+      city: {params.id}
+    </div>
+  );
 }
